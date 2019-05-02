d160b536a89286f55b682e9dfedcf07905c15d5e
Markus Sanders <markus.sanderst@gmail.com>
2019-05-02 22:36:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if !name_hash
+    return nil
+  end
+  
   min = name_hash.first[0]
   minVal = name_hash.first[1]
   name_hash.each {
